fb5aa5d494a26899e2476f2882efd3b2ab02b8cd
ahadhossan <ahadm3016@gmail.com>
2024-07-03 23:56:56 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Info from "./components/Info";
+import Episodes from "./components/Episodes";
+import Videos from "./components/Videos";
+import Details from "./components/Details";
+import Footer from "./components/Footer";
+
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Info />
+      <Episodes />
+      <Videos />
+      <Details />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
